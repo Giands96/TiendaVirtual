bcdc5646fde509493a82b2a847ad6d26bb41a321
Giands96 <dsg4857@gmail.com>
2025-01-03 00:47:06 -0500

    Avance de HomePage y Carrusel de productos

--- src/app/interfaces/products.ts
+++ src/app/interfaces/products.ts
@@ -1,11 +1,11 @@
 export interface Products {
-imagen_url: any;
-categoria_nombre: any;
-nombre: any;
-precio: any;
+    imagen_url: any;
+    categoria_nombre: any;
+    nombre: any;
+    precio: any;
     id: number;
     name: string;
-    description: string;
+    descripcion: string;
     price: number;
     stock: number;
     category: string;
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../../services/product.service';
@@ -12,9 +12,11 @@ import { Products } from '../../interfaces/products';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  @ViewChild('productCarousel', { static: false }) productCarousel!: ElementRef;
   currentIndex = 0;
   autoSliderInterval: any;
   featuredProducts: Products[] = [];
+  products: Products [] = [];
 
   constructor(private productService: ProductService) {}
 
@@ -22,12 +24,16 @@ export class HomePageComponent {
     this.productService.getFeaturedProducts().subscribe((products) => {
       this.featuredProducts = products;
     });
+    this.productService.getProducts().subscribe((products) => {
+      this.products = products;
+    });
 
     // Iniciar el temporizador para el carrusel automático
     this.autoSliderInterval = setInterval(() => {
       this.next();
     }, 3000); // Cambia cada 3 segundos
   }
+
 
   ngOnDestroy() {
     // Limpiar el temporizador al destruir el componente
@@ -37,11 +43,35 @@ export class HomePageComponent {
   next() {
     this.currentIndex = (this.currentIndex + 1) % this.featuredProducts.length;
   }
+  
 
   prev() {
     this.currentIndex = (this.currentIndex - 1 + this.featuredProducts.length) % this.featuredProducts.length;
   }
 
+  nextGeneral() {
+    const carousel = this.productCarousel.nativeElement;
+    const cardWidth = 300 + 24;
+    carousel.scrollLeft += cardWidth;
+  
+    if (carousel.scrollLeft + carousel.clientWidth >= carousel.scrollWidth) {
+      // Reinicia al inicio
+      carousel.scrollLeft = 0;
+    }
+  }
+  
+  prevGeneral() {
+    const carousel = this.productCarousel.nativeElement;
+    const cardWidth = 300 + 10;
+    carousel.scrollLeft -= cardWidth;
+  
+    if (carousel.scrollLeft <= 0) {
+      // Reinicia al final
+      carousel.scrollLeft = carousel.scrollWidth;
+    }
+  }
+
+ 
   viewProduct(productId: number): void {
     console.log("Ver producto con ID:", productId);
     
@@ -55,5 +85,6 @@ export class HomePageComponent {
   setIndex(index: number): void {
     this.currentIndex = index;
   }
+
 
 }
